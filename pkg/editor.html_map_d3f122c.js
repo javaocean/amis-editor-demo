21e536ea8eb138b07accf224aa1780d0b2ba07b0
wibetter <365533093@qq.com>
2022-05-23 03:12:25 +0000

    Deploying to gh-pages from  @ 4e9544d6e390c7ee42c12b7e717a72e426ff2d21 🚀

---
+++ pkg/editor.html_map_d3f122c.js
@@ -0,0 +1,459 @@
+amis.require.resourceMap({
+  "res": {
+    "1820883": {
+      "type": "js",
+      "deps": [
+        "ab783e1"
+      ],
+      "pkg": "p1"
+    },
+    "6245753": {
+      "type": "js",
+      "deps": [
+        "ab783e1"
+      ],
+      "pkg": "p1"
+    },
+    "8586189": {
+      "type": "js",
+      "deps": [
+        "c3d9fbb"
+      ],
+      "pkg": "p4"
+    },
+    "800eaff": {
+      "type": "js",
+      "pkg": "p5"
+    },
+    "de955de": {
+      "type": "js",
+      "pkg": "p5"
+    },
+    "8d0e5e4": {
+      "type": "js",
+      "deps": [
+        "4c5556c"
+      ],
+      "pkg": "p5"
+    },
+    "0d8fb78": {
+      "type": "js",
+      "pkg": "p5"
+    },
+    "bf6b35d": {
+      "type": "js",
+      "pkg": "p5"
+    },
+    "3b1184e": {
+      "type": "js",
+      "deps": [
+        "bf6b35d"
+      ],
+      "pkg": "p5"
+    },
+    "816c31c": {
+      "type": "js",
+      "deps": [
+        "0d8fb78",
+        "bf6b35d",
+        "3b1184e"
+      ],
+      "pkg": "p5"
+    },
+    "e6f7e6c": {
+      "type": "js",
+      "deps": [
+        "icons/pc-preview.svg",
+        "icons/h5-preview.svg"
+      ],
+      "pkg": "p5"
+    },
+    "0cbf1f5": {
+      "type": "js",
+      "deps": [
+        "39ea9ea"
+      ],
+      "pkg": "p5"
+    },
+    "bef2b8d": {
+      "type": "js",
+      "deps": [
+        "39ea9ea"
+      ],
+      "pkg": "p5"
+    },
+    "218f7cb": {
+      "type": "js",
+      "deps": [
+        "39ea9ea",
+        "e6f7e6c",
+        "0cbf1f5",
+        "de955de",
+        "bef2b8d"
+      ],
+      "pkg": "p5"
+    },
+    "4c5556c": {
+      "type": "js",
+      "deps": [
+        "8586189",
+        "11207ed"
+      ],
+      "pkg": "p4"
+    },
+    "c3d9fbb": {
+      "type": "js",
+      "pkg": "p4"
+    },
+    "11207ed": {
+      "type": "js",
+      "pkg": "p4"
+    },
+    "icons/pc-preview.svg": {
+      "url": "/amis-editor-demo/icons/pc-preview_77d4aaf.js",
+      "type": "js"
+    },
+    "icons/h5-preview.svg": {
+      "url": "/amis-editor-demo/icons/h5-preview_92b66fe.js",
+      "type": "js"
+    },
+    "39ea9ea": {
+      "url": "/amis-editor-demo/n/amis-editor/dist/index.min_525636e.js",
+      "type": "js"
+    },
+    "ab783e1": {
+      "type": "js",
+      "pkg": "p1"
+    },
+    "82081a3": {
+      "type": "js",
+      "deps": [
+        "ab783e1"
+      ],
+      "pkg": "p1"
+    },
+    "4abbd07": {
+      "type": "js",
+      "deps": [
+        "ab783e1"
+      ],
+      "pkg": "p1"
+    },
+    "5dc1a56": {
+      "type": "js",
+      "deps": [
+        "ab783e1"
+      ],
+      "pkg": "p1"
+    },
+    "cb23057": {
+      "type": "js",
+      "deps": [
+        "ab783e1"
+      ],
+      "pkg": "p1"
+    },
+    "b3fde94": {
+      "type": "js",
+      "deps": [
+        "ab783e1"
+      ],
+      "pkg": "p1"
+    },
+    "beae209": {
+      "type": "js",
+      "deps": [
+        "ab783e1"
+      ],
+      "pkg": "p1"
+    },
+    "2615c5b": {
+      "type": "js",
+      "deps": [
+        "ab783e1"
+      ],
+      "pkg": "p1"
+    },
+    "343e12f": {
+      "type": "js",
+      "deps": [
+        "ab783e1"
+      ],
+      "pkg": "p1"
+    },
+    "8d57bec": {
+      "type": "js",
+      "deps": [
+        "ab783e1"
+      ],
+      "pkg": "p1"
+    },
+    "82c502a": {
+      "type": "js",
+      "deps": [
+        "ab783e1"
+      ],
+      "pkg": "p1"
+    },
+    "46b34e7": {
+      "type": "js",
+      "deps": [
+        "ab783e1"
+      ],
+      "pkg": "p1"
+    },
+    "2cf4857": {
+      "type": "js",
+      "deps": [
+        "ab783e1"
+      ],
+      "pkg": "p1"
+    },
+    "638b50f": {
+      "type": "js",
+      "deps": [
+        "ab783e1"
+      ],
+      "pkg": "p1"
+    },
+    "8b04589": {
+      "type": "js",
+      "deps": [
+        "ab783e1"
+      ],
+      "pkg": "p1"
+    },
+    "fa32fb9": {
+      "type": "js",
+      "deps": [
+        "ab783e1"
+      ],
+      "pkg": "p1"
+    },
+    "732a468": {
+      "type": "js",
+      "deps": [
+        "ab783e1"
+      ],
+      "pkg": "p1"
+    },
+    "fc39066": {
+      "type": "js",
+      "deps": [
+        "ab783e1"
+      ],
+      "pkg": "p1"
+    },
+    "7604ebf": {
+      "type": "js",
+      "deps": [
+        "ab783e1"
+      ],
+      "pkg": "p1"
+    },
+    "352551a": {
+      "type": "js",
+      "deps": [
+        "ab783e1"
+      ],
+      "pkg": "p1"
+    },
+    "0712a92": {
+      "type": "js",
+      "deps": [
+        "ab783e1"
+      ],
+      "pkg": "p1"
+    },
+    "5b320ac": {
+      "type": "js",
+      "deps": [
+        "ab783e1"
+      ],
+      "pkg": "p1"
+    },
+    "508d226": {
+      "type": "js",
+      "deps": [
+        "ab783e1"
+      ],
+      "pkg": "p1"
+    },
+    "f0b23d9": {
+      "type": "js",
+      "deps": [
+        "ab783e1"
+      ],
+      "pkg": "p1"
+    },
+    "80dd2c7": {
+      "type": "js",
+      "deps": [
+        "ab783e1"
+      ],
+      "pkg": "p1"
+    },
+    "368fbb5": {
+      "type": "js",
+      "deps": [
+        "ab783e1"
+      ],
+      "pkg": "p1"
+    },
+    "b516763": {
+      "type": "js",
+      "deps": [
+        "ab783e1"
+      ],
+      "pkg": "p1"
+    },
+    "f5e2aad": {
+      "type": "js",
+      "deps": [
+        "ab783e1"
+      ],
+      "pkg": "p1"
+    },
+    "a0a75c2": {
+      "type": "js",
+      "deps": [
+        "ab783e1"
+      ],
+      "pkg": "p1"
+    },
+    "40b8028": {
+      "type": "js",
+      "deps": [
+        "ab783e1",
+        "82081a3",
+        "6245753",
+        "4abbd07",
+        "5dc1a56",
+        "cb23057",
+        "b3fde94",
+        "beae209",
+        "2615c5b",
+        "343e12f",
+        "8d57bec",
+        "82c502a",
+        "46b34e7",
+        "2cf4857",
+        "638b50f",
+        "8b04589",
+        "fa32fb9",
+        "732a468",
+        "fc39066",
+        "7604ebf",
+        "352551a",
+        "0712a92",
+        "1820883",
+        "5b320ac",
+        "508d226",
+        "f0b23d9",
+        "80dd2c7",
+        "368fbb5",
+        "b516763",
+        "f5e2aad",
+        "a0a75c2"
+      ],
+      "pkg": "p1"
+    },
+    "27e1ddb": {
+      "type": "js",
+      "pkg": "p2"
+    },
+    "d578068": {
+      "type": "js",
+      "pkg": "p2"
+    },
+    "e369a4b": {
+      "type": "js",
+      "deps": [
+        "d578068"
+      ],
+      "pkg": "p2"
+    },
+    "b0ed2c9": {
+      "type": "js",
+      "pkg": "p2"
+    },
+    "0f70244": {
+      "type": "js",
+      "deps": [
+        "b0ed2c9",
+        "e369a4b"
+      ],
+      "pkg": "p2"
+    },
+    "e1cebc3": {
+      "type": "js",
+      "pkg": "p2"
+    },
+    "e24fcea": {
+      "type": "js",
+      "deps": [
+        "b0ed2c9",
+        "27e1ddb",
+        "0f70244",
+        "e1cebc3"
+      ],
+      "pkg": "p2"
+    },
+    "fd94c97": {
+      "type": "js",
+      "deps": [
+        "27e1ddb"
+      ],
+      "pkg": "p2"
+    },
+    "c9f7b6c": {
+      "type": "js",
+      "deps": [
+        "b0ed2c9",
+        "27e1ddb"
+      ],
+      "pkg": "p2"
+    },
+    "501fbf5": {
+      "type": "js",
+      "deps": [
+        "b0ed2c9",
+        "27e1ddb"
+      ],
+      "pkg": "p2"
+    },
+    "9ef8b9f": {
+      "type": "js",
+      "deps": [
+        "b0ed2c9",
+        "27e1ddb",
+        "fd94c97",
+        "c9f7b6c",
+        "501fbf5"
+      ],
+      "pkg": "p2"
+    },
+    "a3e49d7": {
+      "url": "/amis-editor-demo/n/hls.js/dist/hls_eb8a345.js",
+      "type": "js"
+    }
+  },
+  "pkg": {
+    "p5": {
+      "url": "/amis-editor-demo/pkg/rest_faacb96.js",
+      "type": "js"
+    },
+    "p4": {
+      "url": "/amis-editor-demo/pkg/app_d982c95.js",
+      "type": "js"
+    },
+    "p1": {
+      "url": "/amis-editor-demo/pkg/rich-text_0720d52.js",
+      "type": "js"
+    },
+    "p2": {
+      "url": "/amis-editor-demo/pkg/echarts_c26fe4f.js",
+      "type": "js"
+    }
+  }
+});
